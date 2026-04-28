797b1725e362bc8040a8e072ca768ec8918a8eaf
meeramangukiya <meeramangukiya219@gmail.com>
2026-04-28 18:44:27 +0530

    Pr3

--- Pr3.java
+++ Pr3.java
@@ -1,1 +1,23 @@
 
+import java.util.*;
+
+public class CheckLetter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a single letter: ");
+        char c = sc.next().toLowerCase().charAt(0);
+
+        String v = "aeiou";
+
+        if (Character.isLetter(c))
+            System.out.println("Result: " + (v.contains(c + "") ? "Vowel" : "Consonant"));
+        else
+            System.out.println("Result: Invalid input");
+    }
+}
+*/
+Enter a single letter: A
+Result: Vowel
+  Enter a single letter: b
+Result: Consonant
