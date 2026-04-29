8c7b1d54bd24e2fff2f5c7fb373f2fadcf66dfba
meeramangukiya <meeramangukiya219@gmail.com>
2026-04-29 10:35:45 +0530

    Pr18

---
+++ Pr18.java
@@ -0,0 +1,51 @@
+abstract class Vehicle {
+    abstract String fuelType();
+    abstract int noOfWheels();
+}
+
+class Car extends Vehicle {
+    String fuelType() {
+        return "Petrol/Diesel";
+    }
+
+    int noOfWheels() {
+        return 4;
+    }
+}
+
+class Bike extends Vehicle {
+    String fuelType() {
+        return "Petrol";
+    }
+
+    int noOfWheels() {
+        return 2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vehicle c = new Car();
+        Vehicle b = new Bike();
+
+        System.out.println("Car:");
+        System.out.println("Fuel = " + c.fuelType());
+        System.out.println("Wheels = " + c.noOfWheels());
+
+        System.out.println();
+
+        System.out.println("Bike:");
+        System.out.println("Fuel = " + b.fuelType());
+        System.out.println("Wheels = " + b.noOfWheels());
+    }
+}
+/*
+Car:
+Fuel = Petrol/Diesel
+Wheels = 4
+
+Bike:
+Fuel = Petrol
+Wheels = 2
+*/
